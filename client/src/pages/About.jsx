a97be92be11e8b2c1a037095b9c581a9e8c8be53
Thasneema <thaznitaimoor@gmail.com>
2024-09-26 00:16:29 +0400

    add home and about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,12 @@
 
 function About() {
   return (
-    <div>About</div>
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-700">About</h1>
+      <p className="text-3xl font-bold mb-4 text-slate-800 ">Welcome to our secure and seamless authentication platform! Sign up or log in to access exclusive features, personalized content, and more. Your privacy and security are our top priority, ensuring a smooth and protected experience every time you visit. Get started now!</p>
+      <p className="text-3xl font-bold mb-4 text-slate-800 ">Welcome to our secure and seamless authentication platform! Sign up or log in to access exclusive features, personalized content, and more. Your privacy and security are our top priority, ensuring a smooth and protected experience every time you visit. Get started now!</p>
+      <p className="text-3xl font-bold mb-4 text-slate-800 ">Welcome to our secure and seamless authentication platform! Sign up or log in to access exclusive features, personalized content, and more. Your privacy and security are our top priority, ensuring a smooth and protected experience every time you visit. Get started now!</p>
+    </div>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-700">Welcome to my App</h1>
+      <p className="text-3xl font-bold mb-4 text-slate-800 ">Welcome to our secure and seamless authentication platform! Sign up or log in to access exclusive features, personalized content, and more. Your privacy and security are our top priority, ensuring a smooth and protected experience every time you visit. Get started now!</p>
+      <p className="text-3xl font-bold mb-4 text-slate-800 ">Welcome to our secure and seamless authentication platform! Sign up or log in to access exclusive features, personalized content, and more. Your privacy and security are our top priority, ensuring a smooth and protected experience every time you visit. Get started now!</p>
+      <p className="text-3xl font-bold mb-4 text-slate-800 ">Welcome to our secure and seamless authentication platform! Sign up or log in to access exclusive features, personalized content, and more. Your privacy and security are our top priority, ensuring a smooth and protected experience every time you visit. Get started now!</p>
+    </div>
   )
 }
 
